193ec19ddf657a5e8e7cf09a6247b80156e7e861
schalawut30 <s.chalawut30@gmail.com>
2026-07-12 17:18:34 +0700

    สร้างโครงสร้างใหม่

---
+++ data/examList.js
@@ -0,0 +1,21 @@
+{
+    id: "exam1",
+
+    code: "EX001",
+
+    title: "ชุดที่ 1 : การอบรมโรงเรียนส่งเสริมความปลอดภัย (SPS)",
+
+    description: "แบบทดสอบหลังการอบรม SPS",
+
+    questionFile: "exam1",
+
+    totalQuestion: 20,
+
+    time: 30,
+
+    passScore: 80,
+
+    randomQuestion: true,
+
+    shuffleChoice: true
+}
